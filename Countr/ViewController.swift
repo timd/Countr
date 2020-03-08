de37328106ac2d1a34063ee5ea8984603a18067b
timdotd <tim@duckett.de>
2020-03-08 18:33:23 +0100

    initial commit

--- Countr/ViewController.swift
+++ Countr/ViewController.swift
@@ -9,12 +9,76 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var rowCounter: UILabel!
+    @IBOutlet var stitchCounter: UILabel!
+    
+    var rowCount = 0
+    var stitchCount = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        updateLabels()
+        
+    }
+    
+    func updateLabels() {
+        rowCounter.text = "\(rowCount)"
+        stitchCounter.text = "\(stitchCount)"
     }
 
-
 }
+
+extension ViewController {
+    
+    @IBAction func didTapRowButton(sender: UIButton) {
+        switch sender.tag {
+        case 1000:
+            rowCount += 1
+        case 2000:
+            if rowCount > 0 {
+              rowCount -= 1
+            }
+        default:
+            return
+        }
+    
+        updateLabels()
+        
+    }
+    
+    @IBAction func didTapStitchButton(sender: UIButton) {
+        switch sender.tag {
+        case 1000:
+            stitchCount += 1
+        case 2000:
+            if stitchCount > 0 {
+              stitchCount -= 1
+            }
+        default:
+            return
+        }
+        
+        updateLabels()
+        
+    }
+    
+    @IBAction func didTapResetButton(sender: UIButton) {
+        switch sender.tag {
+        case 1000:
+            rowCount = 0
+        case 2000:
+            stitchCount = 0
+        default:
+            return
+        }
+        
+        updateLabels()
+        
+    }
+    
+}
+
 
