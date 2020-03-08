ea635ecbae896dba5fc825d3b3a5b7f580207008
timdotd <tim@duckett.de>
2020-03-08 17:46:47 +0100

    Initial Commit

---
+++ Countr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Countr
+//
+//  Created by Tim on 08.03.20.
+//  Copyright © 2020 duckett.de. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
